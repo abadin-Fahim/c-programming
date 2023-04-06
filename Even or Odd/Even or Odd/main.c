ba3171fe6971865360b63e99280dfa69f4aa362b
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-06 16:02:51 +0200

    done

--- Even or Odd/Even or Odd/main.c
+++ Even or Odd/Even or Odd/main.c
@@ -1,14 +1,21 @@
 //
 //  main.c
 //  Even or Odd
-//
+//Problem Statement: You need to take one non-negative integer (0 or greater than 0) value as input and tell if it is even or odd.
+
 //  Created by Joynal Abadin Fahim on 06.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n;
+    scanf("%d", &n);
+        if (n%2==0){
+            printf("even\n");
+        }
+        else{
+            printf("odd\n");
+        }
     return 0;
 }
