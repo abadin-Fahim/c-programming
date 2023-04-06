4555017f67ba74e46b7fe9b0fe975a5334e41568
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-06 18:36:24 +0200

    done

--- Print It/Print It/main.c
+++ Print It/Print It/main.c
@@ -15,6 +15,6 @@
 
 int main(int argc, const char * argv[]) {
     
-    
+    printf("Hello, world! I am learning C programming language. ^_^\nProgramming is fun and challenging. /\\/\\/\\\nI want to give my 100%% dedication to learn!\tI will succeed one day.\n");
     return 0;
 }
