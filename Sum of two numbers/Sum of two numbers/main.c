a5be109d90177da1309147a5c9bc8f15e8815db9
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-06 14:59:34 +0200

    done

--- Sum of two numbers/Sum of two numbers/main.c
+++ Sum of two numbers/Sum of two numbers/main.c
@@ -1,14 +1,17 @@
 //
 //  main.c
 //  Sum of two numbers
-//
+//Problem Statement: Take two integers A and B as input and output their summation.
 //  Created by Joynal Abadin Fahim on 06.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int a,b;
+    scanf("%d %d", &a, &b);
+    int sum = a+b;
+    printf("%d\n",sum);
     return 0;
 }
+
