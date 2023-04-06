3dd29229c6411707ba5c6b1ceff4893c9cbcabc3
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-06 18:47:53 +0200

    done

--- Divisible/Divisible/main.c
+++ Divisible/Divisible/main.c
@@ -1,14 +1,21 @@
 //
 //  main.c
 //  Divisible
-//
+//Problem Statement: You will be given a non-negative integer N, you need to tell if this number is divisible by 3 or not. If it is divisible by 3 output "YES" otherwise output "NO" without the quotation mark.
 //  Created by Joynal Abadin Fahim on 06.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n;
+    scanf("%d", &n);
+    if(n>=0 && n%3==0){
+        printf("YES\N");
+    }
+    else{
+        printf("NO\n");
+    }
+    
     return 0;
 }
