25bc284f3339c3310f1f2f0e55bd3fba441f9959
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-06 15:28:54 +0200

    done

--- Divisible By 5 or Not/Divisible By 5 or Not/main.c
+++ Divisible By 5 or Not/Divisible By 5 or Not/main.c
@@ -1,14 +1,23 @@
 //
 //  main.c
 //  Divisible By 5 or Not
-//
+//Problem Statement: You will be given a positive integer N, you need to print from 1 to N and besides the value, print Yes or No. Print Yes if the value is divisible by 5 and print No otherwise.
 //  Created by Joynal Abadin Fahim on 06.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n;
+    scanf("%d", &n);
+    for (int i=1; i<=n; i++) {
+        if (i%5==0){
+            printf("%d Yes\n",i);
+        }
+        else{
+            printf("%d No\n",i);
+        }
+    }
+    
     return 0;
 }
