b5720c43a01db1666edb0ea370609bd02ed2143e
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-06 15:53:35 +0200

    done

--- Mathematical Expression/Mathematical Expression/main.c
+++ Mathematical Expression/Mathematical Expression/main.c
@@ -1,14 +1,23 @@
 //
 //  main.c
 //  Mathematical Expression
-//
+//Problem Statement: You need to take two integer values as input and show the summation, subtraction, multiplication and division in the given format below.
+
 //  Created by Joynal Abadin Fahim on 06.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int num1, num2;
+    scanf("%d %d",&num1,&num2);
+    int sum=num1+num2;
+    int sub=num1-num2;
+    int mul=num1*num2;
+    float div=(float)num1/num2;
+    printf("%d + %d = %d\n",num1,num2, sum);
+    printf("%d - %d = %d\n",num1,num2, sub);
+    printf("%d * %d = %d\n",num1,num2, mul);
+    printf("%d / %d = %0.2f\n",num1,num2, div);
     return 0;
 }
