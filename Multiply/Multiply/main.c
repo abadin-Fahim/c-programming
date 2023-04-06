d6a817228a1dc75faf90518f26224ad0370f9c58
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-06 18:42:28 +0200

    done

--- Multiply/Multiply/main.c
+++ Multiply/Multiply/main.c
@@ -1,14 +1,16 @@
 //
 //  main.c
 //  Multiply
-//
+//Problem Statement: You will be given two integers A and B. You need to give output their multiplication.
 //  Created by Joynal Abadin Fahim on 06.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int a, b;
+    scanf("%d %d", &a,&b);
+    long long int mul = (long long int) a*b;
+    printf("%lld\n",mul);
     return 0;
 }
