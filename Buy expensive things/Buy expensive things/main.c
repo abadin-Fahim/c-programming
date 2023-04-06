e70512fec24114a35c81e8fe94ac4796b28c2d92
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-06 17:57:53 +0200

    buy expensive thinks

---
+++ Buy expensive things/Buy expensive things/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Buy expensive things
+/*Problem Statement:Suppose your friend has told you that she will buy a Gucci Bag if she has 10 thousand taka or more. Otherwise if she has 5 thousand taka or more, she will buy a Levis Bag. Otherwise she will buy Something from New Market. She also told you that, if she could buy the Gucci bag and she has more than 20 thousand taka she will also buy a Gucci Belt.
+
+Now, if you know the amount of money she has, can you tell which item/items she will buy?*/
+
+//  Created by Joynal Abadin Fahim on 06.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    printf("Hello, World!\n");
+    return 0;
+}
