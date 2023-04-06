7a89f9b25034c347ce6c430555d4635caf942b2b
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-06 18:35:51 +0200

    print it

---
+++ Print It/Print It/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  Print It
+/*Problem Statement: Welcome to the "Panta Vat" assignment. In this task you just need to print the following lines as it is.
+ Hello, world! I am learning C programming language. ^_^
+
+ Programming is fun and challenging. /\/\/\
+
+ I want to give my 100% dedication to learn!\tI will succeed one day.
+ */
+//  Created by Joynal Abadin Fahim on 06.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    return 0;
+}
