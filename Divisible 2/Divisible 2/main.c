b1d380f66a406c57cf118a2cd79dccaa623f7315
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-06 18:54:11 +0200

    done

--- Divisible 2/Divisible 2/main.c
+++ Divisible 2/Divisible 2/main.c
@@ -1,14 +1,21 @@
 //
 //  main.c
 //  Divisible 2
-//
+//Problem Statement: You will be given a non-negative integer N, you need to print all numbers from 1 to N that are divisible by both 3 and 7.
 //  Created by Joynal Abadin Fahim on 06.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n;
+    scanf("%d", &n);
+    for (int i=1; i<=n; i++) {
+        if(i%3==0 && i%7==0){
+            printf("%d\n",i);
+        }
+        
+    }
+    
     return 0;
 }
