e47e359ab3fffefa3d7a6c142bbdef9abefc21a1
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-09 19:04:35 +0200

    done

--- Capital or Small or Digit/Capital or Small or Digit/main.c
+++ Capital or Small or Digit/Capital or Small or Digit/main.c
@@ -1,14 +1,26 @@
 //
 //  main.c
 //  Capital or Small or Digit
-//
+//Problem Statement: Given a letter X. Determine whether X is Digit or Alphabet and if it is Alphabet determine if it is Capital Case or Small Case.
 //  Created by Joynal Abadin Fahim on 09.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char x;
+    scanf("%c", &x);
+    if((x>='A' && x<='Z') || (x>='a' && x<='z')){
+        if(x>='A' && x<='Z'){
+            printf("ALPHA\nIS CAPITAL\n");
+        }
+        else{
+            printf("ALPHA\nIS SMALL\n");
+        }
+    }
+    else{
+        printf("IS DIGIT\n");
+    }
+    
     return 0;
 }
