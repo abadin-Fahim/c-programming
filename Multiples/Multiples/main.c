f8213f11a50d95d5d82c825c7b55fdd22f82c4c2
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-09 17:39:39 +0200

    done

--- Multiples/Multiples/main.c
+++ Multiples/Multiples/main.c
@@ -1,14 +1,21 @@
 //
 //  main.c
 //  Multiples
-//
+//Problem Statement: Given two numbers A and B. Print "Multiples" if A is multiple of B or vice versa. Otherwise print "No Multiples".
 //  Created by Joynal Abadin Fahim on 09.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if(a%b==0 || b%a==0){
+        printf("Multiples\n");
+    }
+    else{
+        printf("No Multiples\n");
+    }
+    
     return 0;
 }
