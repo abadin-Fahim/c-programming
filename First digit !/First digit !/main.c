d2a9fbb47a14aea2b5db9899fcf9edc7dd659118
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-09 18:33:49 +0200

    First digit

---
+++ First digit !/First digit !/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  First digit !
+//Problem Statement: 
+//  Created by Joynal Abadin Fahim on 09.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
