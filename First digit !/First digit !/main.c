9feba86ae4cd65279711874c9305b3049f03a369
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-09 18:50:57 +0200

    done

--- First digit !/First digit !/main.c
+++ First digit !/First digit !/main.c
@@ -1,14 +1,27 @@
 //
 //  main.c
 //  First digit !
-//Problem Statement: 
+//Problem Statement: Given a number X. Print "EVEN" if the first digit of X is even number. Otherwise print "ODD".
 //  Created by Joynal Abadin Fahim on 09.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int num;
+    scanf("%d",&num);
+    int div = 0;
+    for (int i=0; i<=num; i++) {
+        div = num/1000;
+        
+    }
+    if(div%2==0){
+        printf("EVEN\n");
+    }
+    else{
+        printf("ODD\n");
+    }
+    
+    
     return 0;
 }
