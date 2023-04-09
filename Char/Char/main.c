a0e8fd336ce79cdc49399b61392856a827f7315c
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-09 18:30:16 +0200

    done

--- Char/Char/main.c
+++ Char/Char/main.c
@@ -1,14 +1,23 @@
 //
 //  main.c
 //  Char
-//
+//Problem Statement: Given a letter X. If the letter is lowercase print the letter after converting it from lowercase letter to uppercase letter. Otherwise print the letter after converting it from uppercase letter to lowercase letter
 //  Created by Joynal Abadin Fahim on 09.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char x;
+    scanf("%c",&x);
+    if(x>='a' && x<='z'){
+        int uppercase = x-32;
+        printf("%c\n", uppercase);
+    }
+    else{
+        int lowercase = x+32;
+        printf("%c\n", lowercase);
+    }
+    
     return 0;
 }
