f07dd37f504bb06d8b88eef21902216727fa5812
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-09 19:05:53 +0200

    Max and Min

---
+++ Max and Min/Max and Min/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Max and Min
+//
+//  Created by Joynal Abadin Fahim on 09.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
