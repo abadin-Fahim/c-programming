607a8533957d87d62b7b04349a5b488109302417
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-09 17:33:09 +0200

    done

--- Welcome for you with Conditions/Welcome for you with Conditions/main.c
+++ Welcome for you with Conditions/Welcome for you with Conditions/main.c
@@ -1,14 +1,21 @@
 //
 //  main.c
 //  Welcome for you with Conditions
-//
+//Problem Statement: Given two numbers A and B. Print "Yes" if A is greater than or equal to B. Otherwise print "No".
 //  Created by Joynal Abadin Fahim on 09.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int a, b;
+    scanf("%d %d",&a,&b);
+    if(a>=b){
+        printf("Yes\n");
+    }
+    else{
+        printf("No\n");
+    }
+    
     return 0;
 }
