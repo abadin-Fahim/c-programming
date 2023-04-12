778c7dd81508203a15f7fc36abc98b176b2a5a4a
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-12 04:25:51 +0200

    done

--- Max and Min/Max and Min/main.c
+++ Max and Min/Max and Min/main.c
@@ -1,14 +1,35 @@
 //
 //  main.c
 //  Max and Min
-//
+//Problem Statement: Given 3 numbers A, B and C, Print the minimum and the maximum numbers.
 //  Created by Joynal Abadin Fahim on 09.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int num1,num2,num3;
+    scanf("%d %d %d", &num1,&num2,&num3);
+    //Minimum number
+    if(num1<=num2 && num1<=num3){
+        printf("%d ", num1);
+        }
+    else if(num2<=num1 && num2<=num3){
+        printf("%d ", num2);
+        }
+    else{
+        printf("%d ", num3);
+        }
+    //Maximum number
+    if(num1>=num2 && num1>=num3){
+        printf("%d", num1);
+        }
+    else if(num2>=num1 && num2>=num3){
+        printf("%d", num2);
+        }
+    else{
+        printf("%d", num3);
+        }
+    
     return 0;
 }
