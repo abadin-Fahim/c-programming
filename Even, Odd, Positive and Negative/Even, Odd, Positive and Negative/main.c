4b48ed21535c866e03382cf37c9355939077d86e
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-12 04:47:25 +0200

    Even, Odd, Positive and Negative

---
+++ Even, Odd, Positive and Negative/Even, Odd, Positive and Negative/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Even, Odd, Positive and Negative
+//Problem Statement: Given N numbers. Count how many of these values are even, odd, positive and negative.
+//  Created by Joynal Abadin Fahim on 12.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
