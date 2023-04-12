19c96176fcc27ea6cbba7befec81c35f9433a508
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-12 04:45:50 +0200

    done

--- Even Numbers/Even Numbers/main.c
+++ Even Numbers/Even Numbers/main.c
@@ -1,14 +1,25 @@
 //
 //  main.c
 //  Even Numbers
-//
+//Problem Statement: Given a number N. Print all even numbers between 1 and N inclusive in separate lines.
 //  Created by Joynal Abadin Fahim on 12.04.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n;
+    scanf("%d", &n);
+    if(n==1){
+        printf("-1\n");
+    }
+    else{
+        for (int i=1; i<=n; i++) {
+            if(i%2==0){
+                printf("%d\n",i);
+            }
+        }
+    }
+    
     return 0;
 }
