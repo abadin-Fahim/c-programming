257302555cc94c46b03d3e2849eabc036b0729e0
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-14 03:06:29 +0200

    update and print

---
+++ update and print/update and print/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  update and print
+//Problem Statement: You will given a positive integer N and an array A of size N. Also you will be given two values X and V. You need to change the value of X'th index to V and then print the array in reverse way.
+//  Created by Joynal Abadin Fahim on 14.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
