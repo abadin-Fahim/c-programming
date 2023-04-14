6a3342294375970b8315ee4e2441ba853be84df1
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-14 03:35:14 +0200

    reverse and even

---
+++ Reverse and even/Reverse and even/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Reverse and even
+//Problem Statement: You will be given a positive integer N and an array A of size N. Suppose, the index starts from 0, then you need to print all the values at even indexes in reverse way.
+//  Created by Joynal Abadin Fahim on 14.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    printf("Hello, World!\n");
+    return 0;
+}
