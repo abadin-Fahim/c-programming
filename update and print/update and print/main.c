0232de82b88b46c2e04f868b81488ae6b2f229a8
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-14 03:33:19 +0200

    done

--- update and print/update and print/main.c
+++ update and print/update and print/main.c
@@ -8,7 +8,20 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n;
+    scanf("%d",&n);
+    int ar_A[n];
+    for (int i=0; i<n; i++) {
+        scanf("%d",&ar_A[i]);
+    }
+    int value_x,value_v;
+    scanf("%d %d", &value_x,&value_v);
+    ar_A[value_x]  = value_v;
+    for (int i=n-1; i>=0; i--) {
+        printf("%d ",ar_A[i]);
+    }
+    
+    
+    
     return 0;
 }
