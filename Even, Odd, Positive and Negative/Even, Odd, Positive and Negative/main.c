e738524cba6483c3ac65d2e3f6df62cd90946a03
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-14 04:48:15 +0200

    fixed password

--- Even, Odd, Positive and Negative/Even, Odd, Positive and Negative/main.c
+++ Even, Odd, Positive and Negative/Even, Odd, Positive and Negative/main.c
@@ -8,7 +8,36 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n;
+    scanf("%d", &n);
+    int even_count=0;
+    int odd_count=0;
+    int positive_count=0;
+    int negative_count=0;
+    int values;
+    for (int i=0; i<n; i++) {
+        scanf("%d",&values);
+        if(values%2==0){
+            even_count++;
+            
+        }
+        else{
+            odd_count++;
+            
+        }
+        if(values>0){
+            positive_count++;
+            
+        }
+        else if(values<0){
+            negative_count++;
+            
+        }
+    }
+    printf("Even: %d\n",even_count);
+    printf("Odd: %d\n",odd_count);
+    printf("Positive: %d\n", positive_count);
+    printf("Negative: %d\n", negative_count);
+    
     return 0;
 }
---
+++ Fixed Password/Fixed Password/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Fixed Password
+//Problem Statement: Given multiple lines each line contains a number X which is a password. Print "Wrong" if the password is incorrect otherwise, print "Correct" and terminate the program.
+//  Created by Joynal Abadin Fahim on 14.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
