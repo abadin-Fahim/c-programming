c68aaa3aa23c361d8e2497fc5730a3487a997639
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-14 02:18:34 +0200

    Print It_2

--- Even, Odd, Positive and Negative/Even, Odd, Positive and Negative/main.c
+++ Even, Odd, Positive and Negative/Even, Odd, Positive and Negative/main.c
@@ -8,7 +8,36 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n;
+    scanf("%d", &n);
+    int even_count=0;
+    int odd_count=0;
+    int positive_count=0;
+    int negative_count=0;
+   // int a;
+    for (int i=0; i<n; i++) {
+        scanf("%d ",&i);
+        if(i%2==0){
+            even_count++;
+            
+        }
+        else{
+            odd_count++;
+            
+        }
+        if(i>0){
+            positive_count++;
+            
+        }
+        else if(i<0){
+            negative_count++;
+            
+        }
+    }
+    printf("Even: %d\n",even_count);
+    printf("Odd: %d\n",odd_count);
+    printf("Positive: %d\n", positive_count);
+    printf("Negative: %d\n", negative_count);
+    
     return 0;
 }
---
+++ Print It_2/Print It_2/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Print It_2
+//Problem Statement: You will be given a positive integer N. You need to print "I Love Assignment" N times without the quotation mark.
+
+
+//  Created by Joynal Abadin Fahim on 14.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
