fca11dcf6ffcc4919ea6f91bc8ed76b7fd574d5c
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-18 02:08:19 +0200

    MAX

---
+++ Max/Max/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  Max
+//Problem Statement: Given a number N, and N numbers, find maximum number in these N numbers.
+//  Created by Joynal Abadin Fahim on 18.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+   
+    return 0;
+}
