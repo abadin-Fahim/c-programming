b3b77dccbfa7ef1e8e808c228be13fffd9181de3
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-18 03:35:53 +0200

    done

--- Ali Baba and Puzzles/Ali Baba and Puzzles/main.c
+++ Ali Baba and Puzzles/Ali Baba and Puzzles/main.c
@@ -13,7 +13,28 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int a,b,c,d;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    if(a+b-c==d){
+        printf("YES\n");
+    }
+    else if(a-b+c==d){
+        printf("YES\n");
+    }
+    else if(a+b*c==d){
+        printf("YES\n");
+    }
+    else if(a*b+c==d){
+        printf("YES\n");
+    }
+    else if(a-b*c==d){
+        printf("YES\n");
+    }
+    else if(a*b-c==d){
+        printf("YES\n");
+    }
+    else{
+        printf("NO\n");
+    }
     return 0;
 }
