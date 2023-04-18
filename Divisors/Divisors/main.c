da152024f27caf5d43aa51f271217f630c6d0d40
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-18 03:44:43 +0200

    done

--- Divisors/Divisors/main.c
+++ Divisors/Divisors/main.c
@@ -8,6 +8,14 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    
+    int n;
+    scanf("%d", &n);
+    for (int i=1; i<=n; i++) {
+        int divisors = n%i;
+        if(divisors==0){
+            printf("%d\n",i);
+        }
+        
+    }
     return 0;
 }
