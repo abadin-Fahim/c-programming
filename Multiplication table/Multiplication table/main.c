5f4af34d6a9231b9f272787629c5f8c2ddb7a433
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-18 02:31:52 +0200

    Multiplication table

---
+++ Multiplication table/Multiplication table/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Multiplication table
+//Problem Statement: Given a number N. Print the maltiplication table of the number from 1 to 12
+//  Created by Joynal Abadin Fahim on 18.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
