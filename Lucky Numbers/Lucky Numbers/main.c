62c8bc89aa1f48932e7b251a7e6f3f87b83e5303
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-18 04:04:37 +0200

    Lucky Numbers

---
+++ Lucky Numbers/Lucky Numbers/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  Lucky Numbers
+/*
+ Problem Statement: A number of two digits is lucky if one of its digits is divisible by the other.
+ 
+ For example, 39, 82, and 55 are lucky, while 79 and 43 are not.
+
+ Given a number between 10 and 99, determine whether it is lucky or not.
+ */
+//  Created by Joynal Abadin Fahim on 18.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
