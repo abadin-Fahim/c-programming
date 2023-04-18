bf89e0c92fd4aba3e93bb9b4892a8f952aed5bde
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-18 03:25:56 +0200

    Ali Baba and Puzzles

---
+++ Ali Baba and Puzzles/Ali Baba and Puzzles/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Ali Baba and Puzzles
+/*
+ Problem Statement: One day, Ali Baba had an easy puzzle that he couldn't solve. The puzzle consisted of 4 numbers and his task was to check whether he could get the fourth number using arithmetic operators (+,−,×) between the other three numbers; so that each operator is used only once.
+𝑎  ◻   𝑏  ◻   𝑐  =   𝑑
+ Can you solve this tricky puzzle for him?
+
+ */
+//  Created by Joynal Abadin Fahim on 18.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
