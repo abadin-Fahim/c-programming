a055fc9c7f69522130b6d8429c75afea3f341094
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-18 03:37:38 +0200

    Divisors

---
+++ Divisors/Divisors/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  Divisors
+//Problem Statement: Given a number N. Print all the divisors of N in ascending order.
+//  Created by Joynal Abadin Fahim on 18.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
