6d96f69a354451d9ba0c91b98716cd474ac051a1
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-18 02:29:56 +0200

    alternative way

--- Max/Max/main.c
+++ Max/Max/main.c
@@ -6,18 +6,36 @@
 //
 
 #include <stdio.h>
+#include <limits.h>
 
 int main(int argc, const char * argv[]) {
+    /*
+     int n;
+     scanf("%d", &n);
+     int a,max=0;
+     for (int i=0; i<n; i++) {
+         scanf("%d", &a);
+         if(a>max){
+             max = a;
+         }
+         
+     }
+     printf("%d",max);
+     */
+    //alternative
     int n;
     scanf("%d", &n);
-    int a,max=0;
+    int a,max=INT_MIN,min=INT_MAX;
     for (int i=0; i<n; i++) {
         scanf("%d", &a);
         if(a>max){
             max = a;
         }
+        if(a<min){
+            min = a;
+        }
         
     }
-    printf("%d",max);
+    printf("%d %d\n",max,min);
     return 0;
 }
