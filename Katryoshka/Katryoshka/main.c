2596557fb688daf2d2f45e5f24f459789811dfe7
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-18 04:18:52 +0200

    Katryoshka

---
+++ Katryoshka/Katryoshka/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Katryoshka
+/*
+ Problem Statement : The Egyptian football team will be in Russia for the World Cup. Of course, they all would like to buy souvenirs for their families. Luckily, they met the king of souvenirs Matryoshka who is famous for his masterpiece Katryoshka. He makes it using different wooden pieces: eyes, mouths and bodies. He can form a nice Katryoshka using one of the following combinations:
+ 
+ 1.Two eyes and one body.
+ 2.Two eyes, one mouth, and one body.
+ 3.One eye, one mouth, and one body.
+ If the king has 𝑛 eyes, 𝑚 mouths and 𝑘 bodies, what is the largest number of Katryoshkas he can make?
+ */
+//  Created by Joynal Abadin Fahim on 18.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int n,m,k;
+    
+    return 0;
+}
