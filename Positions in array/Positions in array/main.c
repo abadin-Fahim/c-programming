b465dba7944f06ac07d962d01044dcb21dc4b59c
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-19 23:42:27 +0200

    Positions in array

---
+++ Positions in array/Positions in array/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Positions in array
+/*
+ Problem Statement: Given a number N and an array A of N numbers. Print all array positions that store a number less than or equal to 10 and the number stored in that position.
+ */
+//  Created by Joynal Abadin Fahim on 19.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
