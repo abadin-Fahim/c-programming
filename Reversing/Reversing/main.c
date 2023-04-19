f8e4c2c090f52a659095962e70c93fa93ea942fd
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-20 00:15:03 +0200

    Reversing

---
+++ Reversing/Reversing/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Reversing
+/*
+ Problem Statement: Given a number N and an array A of N numbers. Print the array in a reversed order.
+ 
+ Note:
+
+ *Don't use built-in-functions.
+ */
+//  Created by Joynal Abadin Fahim on 20.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
