e665b518d0b83827124a0b2a8fd3112aa723a3d1
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-19 23:49:38 +0200

    solved

--- Positions in array/Positions in array/main.c
+++ Positions in array/Positions in array/main.c
@@ -10,7 +10,17 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n;
+    scanf("%d",&n);
+    int ar[n];
+    for (int i=0; i<n; i++) {
+        scanf("%d",&ar[i]);
+        
+    }
+    for (int i=0; i<n; i++) {
+        if(ar[i]<=10){
+            printf("A[%d] = %d\n",i,ar[i]);
+        }
+    }
     return 0;
 }
