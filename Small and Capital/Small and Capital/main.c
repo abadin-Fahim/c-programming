1d6706bc243e473f94b70aa98f6faa1f3c5170b3
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-20 20:11:20 +0200

    Small and Capital

---
+++ Small and Capital/Small and Capital/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Small and Capital
+//Problem Statement: You will be given a string S. The string will contain both small and capital English alphabets only. You need to tell how many of them are capital alphabets and how many are small alphabets.
+//  Created by Joynal Abadin Fahim on 20.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
