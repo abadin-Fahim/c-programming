5d0d184e1636eb37787d30f9dc022f9d2d8b18e0
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-20 23:33:58 +0200

    find It

---
+++ Find It/Find It/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  Find It
+//Problem Statement: You will given an integer array A and the size N. You will also be given an integer value X. You need to tell how many times X was appeared in the array.
+//  Created by Joynal Abadin Fahim on 20.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
