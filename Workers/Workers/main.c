9a3bd09e3f37c319884c8af6a4372bd76f5ad70f
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-20 20:01:26 +0200

    Workers

---
+++ Workers/Workers/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Workers
+/*Problem Statement:Suppose there are M1 workers who can complete a work in D days. Unfortunately, some of them became sick before the start of the work, and now there are M2 workers. Can you tell how many days it will take for them to complete the work?
+ 
+ Note: If the answer is a floating value, print the integer part of the answer.
+ */
+//  Created by Joynal Abadin Fahim on 20.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
