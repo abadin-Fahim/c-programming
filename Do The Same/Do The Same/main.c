eb0201b81846cc7894e65d843889392e6e8cd61e
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-20 23:20:02 +0200

    Do The Same

---
+++ Do The Same/Do The Same/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Do The Same
+/*Problem Statement: You will be given two positive integer N and K. You need to print from 1 to N, and you need to do this K times.
+ 
+ Please look at the sample input output.
+
+
+ */
+//  Created by Joynal Abadin Fahim on 20.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
