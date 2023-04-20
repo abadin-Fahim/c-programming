f6a86367410cd0842f1b854388cf9fa2df6fef1d
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-20 23:41:19 +0200

    solved

--- Find It/Find It/main.c
+++ Find It/Find It/main.c
@@ -8,6 +8,19 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    
+    int n;
+    scanf("%d",&n);
+    int ar[n];
+    for (int i=0; i<n; i++) {
+        scanf("%d",&ar[i]);
+    }
+    int x, count=0;
+    scanf("%d",&x);
+    for (int i=0; i<n; i++) {
+        if(ar[i]==x){
+            count++;
+        }
+    }
+    printf("%d\n",count);
     return 0;
 }
