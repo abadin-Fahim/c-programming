036d23685130b4a344111d8743d5c225de585f66
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-21 00:35:30 +0200

    solved

--- Count III/Count III/main.c
+++ Count III/Count III/main.c
@@ -6,9 +6,17 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char str[1000];
+    scanf("%s",str);
+    int count[26]={0};
+    for (int i=0; i<strlen(str); i++) {
+        count[str[i]-'a']++;
+    }
+    for (int i=0; i<26; i++) {
+        printf("%c - %d\n",i+'a',count[i]);
+    }
     return 0;
 }
