050cba82bcef2aaa3a4dd39dc127c3b84f159a5e
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-04-20 19:25:31 +0200

    Count 2

---
+++ Count II/Count II/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Count II
+/*Problem Statement : You will be given a string S as input contains only small English alphabets. You need to tell the number of vowels in it. The string will not contain any spaces.
+ */
+//  Created by Joynal Abadin Fahim on 20.04.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
