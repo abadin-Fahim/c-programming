81bb3d078a5a0719448e419404ea0d3127410cae
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-05 03:49:16 +0200

    Pattern_2

---
+++ Pattern_2/Pattern_2/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  Pattern_2
+//Problem Statement: Write a C program to take positive integer N as input and print a pattern shown in the sample input output.
+//  Created by Joynal Abadin Fahim on 05.05.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
