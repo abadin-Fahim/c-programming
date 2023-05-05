18aa4b3d3fb8ead963a85ad9d62310c53d20c783
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-05 03:58:24 +0200

    solved

--- Pattern_2/Pattern_2/main.c
+++ Pattern_2/Pattern_2/main.c
@@ -8,6 +8,19 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    
+    int n,space,k=1;
+    scanf("%d",&n);
+    space=n-1;
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=space; j++) {
+            printf(" ");
+        }
+        for (int j=1; j<=k; j++) {
+            printf("%d",i);
+        }
+        space--;
+        k++;
+        printf("\n");
+    }
     return 0;
 }
