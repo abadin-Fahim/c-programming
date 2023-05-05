595c194561018d74f652389288d978b4cee4fec5
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-05 04:01:42 +0200

    count before zero

---
+++ Count before zero/Count before zero/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Count before zero
+//
+//  Created by Joynal Abadin Fahim on 05.05.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
