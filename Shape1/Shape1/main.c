d29a09739a0c6ca75468f7eb41960d5ce492fc07
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-07 01:57:55 +0200

    Shape1

---
+++ Shape1/Shape1/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  Shape1
+//Problem Statement: Given a number N. Print a face down right angled triangle that has N rows.
+//  Created by Joynal Abadin Fahim on 07.05.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
