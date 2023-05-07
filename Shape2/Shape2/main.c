9b157c380224c7cb8186df5c7576538d6fc9190c
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-07 02:23:27 +0200

    solved

--- Shape2/Shape2/main.c
+++ Shape2/Shape2/main.c
@@ -8,6 +8,20 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    
+    int n,space,k;
+    scanf("%d",&n);
+    space=n-1;
+    k=1;
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=space; j++) {
+            printf(" ");
+        }
+        for (int j=1; j<=k; j++) {
+            printf("*");
+        }
+        space--;
+        k+=2;
+        printf("\n");
+    }
     return 0;
 }
