9d06971d6f5b219445b5ac12e3b9f3251ee709c2
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-07 02:05:21 +0200

    solved

--- Shape1/Shape1/main.c
+++ Shape1/Shape1/main.c
@@ -8,6 +8,15 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    
+    int n,space,k;
+    scanf("%d",&n);
+    k=n;
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=k; j++) {
+            printf("*");
+        }
+        k--;
+        printf("\n");
+    }
     return 0;
 }
