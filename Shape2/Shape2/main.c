8ac19753bfc477d094d70d79b6e0b48351fe55e2
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-07 02:07:54 +0200

    Shape2

---
+++ Shape2/Shape2/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  Shape2
+//Problem Statement: iven a number N. Print a pyramid that has N rows.
+//  Created by Joynal Abadin Fahim on 07.05.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
