97f0382ca66d1e2abd0e506e53b055a193faffe6
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-07 03:45:30 +0200

    not solved yet

--- Shape3/Shape3/main.c
+++ Shape3/Shape3/main.c
@@ -8,6 +8,33 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
+    int n,space,k;
+    scanf("%d",&n);
+    space=n-1;
+    k=1;
+    for (int i=1; i<=(2*n); i++) {
     
+        for (int j=1; j<=space; j++) {
+            printf(" ");
+        }
+        
+        for (int j=1; j<=k; j++) {
+            printf("*");
+            }
+        if(i<=n-1){
+            space--;
+            k+=2;
+        }
+        else if (i==n){
+            space=1;
+            k-=2;
+        }
+        else{
+            space++;
+            k-=2;
+        }
+        
+        printf("\n");
+    }
     return 0;
 }
