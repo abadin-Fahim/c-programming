33fcf632170ba098da2ccdd45023bbea8cb24592
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-07 02:26:20 +0200

    Shape3

---
+++ Shape3/Shape3/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  Shape3
+//Problem Statement: Given a number N. Print a diamond that has 2N rows.
+//  Created by Joynal Abadin Fahim on 07.05.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
