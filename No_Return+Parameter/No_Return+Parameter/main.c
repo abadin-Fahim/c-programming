45ce7485899f441dbea26a716772a2680d7ceae6
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-08 02:23:33 +0200

    No_Return+Parameter

---
+++ No_Return+Parameter/No_Return+Parameter/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  No_Return+Parameter
+//
+//  Created by Joynal Abadin Fahim on 08.05.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
