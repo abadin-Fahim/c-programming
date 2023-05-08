e57957df1a4fa17b5251866b151b21789c372e46
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-08 02:00:05 +0200

    Has Return + Parameter & solved

---
+++ Has_Return+Parameter/Has_Return+Parameter/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Has_Return+Parameter
+//
+//  Created by Joynal Abadin Fahim on 08.05.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
