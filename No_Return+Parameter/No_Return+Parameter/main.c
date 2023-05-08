a0dc0e2fdbf70d46a0a37105e1e5d66bc9c12272
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-08 02:32:55 +0200

    solved

--- No_Return+Parameter/No_Return+Parameter/main.c
+++ No_Return+Parameter/No_Return+Parameter/main.c
@@ -7,8 +7,35 @@
 
 #include <stdio.h>
 
+void summation(int a , int b){
+    int sum = a + b;
+    printf("Sum = %d\n",sum);
+    return;
+}
+void substract(int a , int b){
+    int sub = a - b;
+    printf("Sub = %d\n",sub);
+    return;
+}
+void multiply(int a , int b){
+    int product = a * b;
+    printf("Product = %d\n",product);
+    return;
+}
+void division(int a , int b){
+    int div = a / b;
+    printf("Division = %d\n",div);
+    return;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int x,y;
+    scanf("%d %d", &x, &y);
+    summation(x, y);
+    substract(x, y);
+    multiply(x, y);
+    division(x, y);
+    
     return 0;
 }
