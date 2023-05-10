f0c04f14a5bed8c1dc61fffd48bd1917f7b7e202
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-11 01:41:01 +0200

    Print Recursion

---
+++ Print Recursion/Print Recursion/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Print Recursion
+//Problem Statement: Given a number 𝑁. Print "I love Recursion" 𝑁 times.
+//  Created by Joynal Abadin Fahim on 11.05.23.
+//
+
+#include <stdio.h>
+
+int recursion(int n){
+    
+}
+
+int main(int argc, const char * argv[]) {
+    int x;
+    scanf("%d",&x);
+    recursion();
+    return 0;
+}
