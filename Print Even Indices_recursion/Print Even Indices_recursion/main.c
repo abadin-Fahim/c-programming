a5a529c70f6d7a9eb4d5328b0a7a92d7593394d8
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-11 03:40:16 +0200

    solved

--- Print Even Indices_recursion/Print Even Indices_recursion/main.c
+++ Print Even Indices_recursion/Print Even Indices_recursion/main.c
@@ -1,14 +1,31 @@
 //
 //  main.c
 //  Print Even Indices_recursion
-//
+//Problem Statement: Given a number N and an array A of N numbers. Print the numbers in even indices in a reversed order.
 //  Created by Joynal Abadin Fahim on 11.05.23.
 //
 
 #include <stdio.h>
 
+void even_indices(int ar[], int n, int i){
+    if(i<0){
+        return;
+    }
+    
+    else if (i%2==0){
+        printf("%d ",ar[i]);
+    }
+    even_indices(ar, n, i-1);
+    
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int x;
+    scanf("%d",&x);
+    int ar[x];
+    for (int i=0; i<x; i++) {
+        scanf("%d",&ar[i]);
+    }
+    even_indices(ar,x,x-1);
     return 0;
 }
