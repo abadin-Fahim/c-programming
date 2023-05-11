a54026d7993d99604c9ade455c5e6839670fcaa3
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-11 02:37:58 +0200

    Print Even Indices

---
+++ Print Even Indices_recursion/Print Even Indices_recursion/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Print Even Indices_recursion
+//
+//  Created by Joynal Abadin Fahim on 11.05.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
