a85e15970bb7b83cc3d3856d73cf57728338b589
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-11 15:27:18 +0200

    Lucky Array

---
+++ Lucky Array/Lucky Array/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lucky Array
+//
+//  Created by Joynal Abadin Fahim on 11.05.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
