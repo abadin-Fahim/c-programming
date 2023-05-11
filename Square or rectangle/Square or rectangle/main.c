d65f37394f6847ac25925e15f7eb53742f659b02
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-11 05:13:47 +0200

    solved

--- Square or rectangle/Square or rectangle/main.c
+++ Square or rectangle/Square or rectangle/main.c
@@ -1,14 +1,25 @@
 //
 //  main.c
 //  Square or rectangle
-//
+//Problem Statement: Given the width and the height of a shape determine whether it's for a square or a rectangle?
 //  Created by Joynal Abadin Fahim on 11.05.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int test_case;
+    scanf("%d",&test_case);
+    int width,height;
+    for (int i=0; i<test_case; i++) {
+        scanf("%d %d", &width, &height);
+        if(width == height){
+            printf("Square\n");
+        }
+        else{
+            printf("Rectangle\n");
+        }
+    }
+    
     return 0;
 }
