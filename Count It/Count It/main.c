c915f07365087fb3cef1d396f9c4494fcbab0df8
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-11 19:02:38 +0200

    solved

--- Count It/Count It/main.c
+++ Count It/Count It/main.c
@@ -1,14 +1,40 @@
 //
 //  main.c
 //  Count It
-//
+//Problem Statement: You will be given a string S consisting of small alphabets, capital alphabets and spaces. You need to count number of small alphabets, capital alphabets and spaces in the string S.
 //  Created by Joynal Abadin Fahim on 11.05.23.
 //
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char s[1001];
+    fgets(s, 1001, stdin);
+    int count_capital=0;
+    for (int i=0; i<strlen(s); i++) {
+        if(s[i]>='A' && s[i]<='Z'){
+            count_capital++;
+        }
+    }
+    
+    printf("Capital - %d\n",count_capital);
+    int count_small=0;
+    for (int i=0; i<strlen(s); i++) {
+        if(s[i]>='a' && s[i]<='z'){
+            count_small++;
+        }
+    }
+    printf("Small - %d\n",count_small);
+    
+    int count_spaces=0;
+    for (int i=0; i<strlen(s); i++) {
+        if(s[i]==' '){
+            count_spaces++;
+        }
+    }
+    printf("Spaces - %d\n",count_spaces);
+    
+    
     return 0;
 }
