5e2829e46aa7f5290c5b85c50d62f6c730bc41de
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-11 02:19:31 +0200

    solved

--- Print from 1 to N _Recursion/Print from 1 to N _Recursion/main.c
+++ Print from 1 to N _Recursion/Print from 1 to N _Recursion/main.c
@@ -1,14 +1,25 @@
 //
 //  main.c
 //  Print from 1 to N _Recursion
-//
+//Problem Statement: Given a number N. Print numbers from 1 to N in separate lines.
 //  Created by Joynal Abadin Fahim on 11.05.23.
 //
 
 #include <stdio.h>
 
+void print_numbers(int n){
+    
+    if(n<=0){
+        return;
+    }
+    print_numbers(n-1);
+    printf("%d\n",n);
+    
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int x;
+    scanf("%d",&x);
+    print_numbers(x);
     return 0;
 }
