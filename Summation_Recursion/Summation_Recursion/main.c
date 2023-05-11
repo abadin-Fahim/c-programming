df3ccbf0cfcd1fe4097d1a19a9d58d0dbf5ef296
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-11 04:42:36 +0200

    not finised

--- Summation_Recursion/Summation_Recursion/main.c
+++ Summation_Recursion/Summation_Recursion/main.c
@@ -6,9 +6,26 @@
 //
 
 #include <stdio.h>
+int sum=0;
+void summation(int ar[], int n, int i){
+    
+    if(i==n){
+        return;
+    }
+    sum += ar[i];
+    
+    summation(ar, n, i+1);
+    
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int x;
+    scanf("%d",&x);
+    int ar[x];
+    for (int i=0; i<x; i++) {
+        scanf("%d",&ar[i]);
+    }
+    summation(ar, x, 0);
+    printf("%d",sum);
     return 0;
 }
