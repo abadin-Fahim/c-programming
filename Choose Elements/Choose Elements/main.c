cb00b7599905d56bf55db664db3f75b52a14b7db
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-11 16:21:09 +0200

    Choose Elements

---
+++ Choose Elements/Choose Elements/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Choose Elements
+//
+//  Created by Joynal Abadin Fahim on 11.05.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
