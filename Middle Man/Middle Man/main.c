82e6b08b437988d66986e3e87c4ef30261ae0c03
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-12 01:12:39 +0200

    Middle Man

---
+++ Middle Man/Middle Man/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Middle Man
+//
+//  Created by Joynal Abadin Fahim on 12.05.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
