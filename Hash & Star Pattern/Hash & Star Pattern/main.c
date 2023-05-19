3e3dea02c0f7c5dd3059425758d2e12fe7094333
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-19 17:48:49 +0200

    solved

--- Hash & Star Pattern/Hash & Star Pattern/main.c
+++ Hash & Star Pattern/Hash & Star Pattern/main.c
@@ -1,14 +1,28 @@
 //
 //  main.c
 //  Hash & Star Pattern
-//
+//Problem Statement: Write a C program that will take an positive Odd integer N and create this pattern for the corresponding inputs. See the sample input output for understanding the pattern.
 //  Created by Joynal Abadin Fahim on 19.05.23.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n;
+        scanf("%d",&n);
+        int mid_row=(n+1)/2;
+        for (int i=1; i<=n; i++) {
+            
+            for (int j=1; j<=n; j++) {
+                if(i==mid_row || j==mid_row){
+                    printf("#");
+                }
+                else{
+                    printf("*");
+                }
+            }
+        
+            printf("\n");
+        }
     return 0;
 }
