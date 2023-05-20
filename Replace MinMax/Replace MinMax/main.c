05ae810c751134d918e189c6a11f19c732f81c12
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-05-21 00:42:17 +0200

    Replace MinMax

---
+++ Replace MinMax/Replace MinMax/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Replace MinMax
+/*
+ Problem Statement: Given a number N and an array A of N numbers. Print the array after doing the following operations:
+ 
+ Find minimum number in these numbers.
+ Find maximum number in these numbers.
+ Swap minimum number with maximum number.
+ */
+//  Created by Joynal Abadin Fahim on 21.05.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
